c2838d22a63611c1764f34af7998e98ad2409734
SangJee Park <sjpark3927@gmail.com>
2024-08-04 02:15:46 +0900

    banner

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Banner from './component/Banner/Banner'
+
+//1.배너 => popular 영화를 들고와서 첫번째 아이템 보여주기
+//2. popular movie
+//3. top rated movie
+//4. upcoming movie
 
 const Homepage = () => {
   return (
     <div>
-      homepage
+      <Banner />
     </div>
   )
 }
-
+ 
 export default Homepage
